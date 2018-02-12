e3e193cada702ea156639fab7aef27fe3ef86555
Govinda lal Banjade <32372320+GovindaBanjade@users.noreply.github.com>
2018-02-12 18:03:01 -0500

    Added a comment

--- analyze.R
+++ analyze.R
@@ -1,6 +1,11 @@
+
+
 # import the data file
 df = read.csv('data/inflammation-01.csv', header = FALSE)
 avg_day_inflammation <- apply(df, 2, mean)
 plot(avg_day_inflammation, ann = TRUE, main = "Dan")
 
 summary(df)
+
+
+# Will be working on this project tonight.
